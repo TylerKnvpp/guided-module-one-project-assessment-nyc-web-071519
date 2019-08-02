658c29e85bc1cf9923076e84b7a0ee847c8430df
Tyler Knapp <tgknapp11@gmail.com>
2019-08-02 09:25:24 -0400

    updated environment.rb to remove sql messages

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ require 'colorized_string'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app/models'
 require_all 'app/controllers'
